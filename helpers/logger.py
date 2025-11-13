65c8c62aed1991301c2fdaf43afa07f0b4732714
Bibek Kumar Mahato <bibekkumar723129@gmail.com>
2025-11-13 19:11:25 +0530

    Create logger.py

---
+++ helpers/logger.py
@@ -0,0 +1,13 @@
+import logging
+from config import LOG_FILE
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s | %(levelname)s | %(message)s',
+    handlers=[
+        logging.FileHandler(LOG_FILE),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
