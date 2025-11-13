8b604dfdb14b4b2aaf72488ac357a8e01ca2202b
Bibek Kumar Mahato <bibekkumar723129@gmail.com>
2025-11-13 20:09:38 +0530

    Update logger.py

--- helpers/logger.py
+++ helpers/logger.py
@@ -1,9 +1,14 @@
 import logging
+import os
 from config import LOG_FILE
+
+# Auto-create logs directory
+log_dir = os.path.dirname(LOG_FILE)
+os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s | %(levelname)s | %(message)s',
+    format="%(asctime)s | %(levelname)s | %(message)s",
     handlers=[
         logging.FileHandler(LOG_FILE),
         logging.StreamHandler()
